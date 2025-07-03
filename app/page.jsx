c46260dd8a9a0daf62f2047ee469f6886375f10d
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-07-04 00:02:15 +0530

    page update

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { FoDownload } from "react-icons/fi";
+
+const Home = () => {
+  return <section className="h-full">
+    <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        {/*text*/}
+        <div className="text-center xl:text-left">
+          <span className="text-xl">Software Engineer</span>
+          <h1 className="h1 mb-6">
+            Hello I'm <br /> <span className="text-accent">Siwanthaka Savinda</span>
+          </h1>
+          <p className="max-w-[500px] mb-9 text-white/80">
+            I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.
+          </p>
+        </div>
+        {/*photo*/}
+        <div>photo</div>
+      </div>
+    </div>
+  </section>;
+};
+
+export default Home;

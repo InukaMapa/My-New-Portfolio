bd34e1034d98f7b10ea7f3ad96c416e9ca2395fd
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-07-20 12:28:18 +0530

    update service page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services =[
+    {
+        num:"01",
+        title:"Web Development",
+        description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
+        href:""
+    },
+    {
+        num:"01",
+        title:"Web Development",
+        description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
+        href:""
+    },
+    {
+         num:"01",
+        title:"Web Development",
+        description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
+        href:""
+        
+    }
+]
+
+
+
 const Services  = () => {
     return <div>services page</div>;
     

b3ce6afcbf2f0e513e048179ab1eedc01c21278e
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-07-20 12:47:05 +0530

    service page update

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -11,24 +11,49 @@ const services =[
         href:""
     },
     {
-        num:"01",
-        title:"Web Development",
+        num:"02",
+        title:"UI/UX Design",
         description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
         href:""
     },
     {
-         num:"01",
-        title:"Web Development",
+         num:"03",
+        title:"Logo Design",
         description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
         href:""
         
-    }
-]
+    },
+     {
+         num:"04",
+        title:"SEO",
+        description:"I specialize in creating elegant digital experiences and have strong proficiency in a wide range of programming languages and technologies.",
+        href:""
+        
+    },
+    
+];
 
-
+import { motion } from "framer-motion";
 
 const Services  = () => {
-    return <div>services page</div>;
+    return(
+         <section className="min-h-[88vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div
+                initial={{opacity:0}}
+                animate={{
+                    opacity:1,
+                    transition:{ delay :2.4, duration: 0.4, ease:"easeIn"},
+                }}
+                >
+                    {services.map((service, index)=>{
+
+                    })}
+                </motion.div>
+            </div>
+ 
+         </section>
+         );
     
 };
 

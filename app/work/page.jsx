7dbef82c5ecfc648e93968857bef249a79112444
Inuka Mapa <139098767+InukaMapa@users.noreply.github.com>
2025-08-02 23:22:12 +0530

    Update page.jsx

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -59,11 +59,45 @@ const Work = () => {
       className="min-h-[80vh] flex flex-col justify-center pv-12 xl:px-0">
         <div className="container mx-auto">
             <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-            <div>text</div>
-            <div>slider</div>
+                 <div className="w-full xl:h-[460px] flex flex-col
+                 xl:justify-between order-2 xl:order-none">
+                    <div>
+                        {/*outlin num*/}
+                      <div className="text-8xl leading-none font-extrabold text-transparent
+                      text-outline">
+                        {project.num}
+                        </div> 
+                        {/*project category*/}
+                        <h2 className="text-[42px] font-bold leading -none text-white
+                        group-hover:text-accent transition-all duration-500 capitalize">
+                            {project.category}project
+                            </h2> 
+                        {/*project description*/}
+                        <p className="text-white/60">{project.description}</p>
+                        {/*stack*/}
+                        <ul className="flex gap-4">
+                            {project.stack.map((item, index) =>{
+                                return(
+                                    <li key={index} className="text-xl text-accent">
+                                        {item.name}
+                                        {/*remove the last coma*/}
+                                        {index !== project.stack.length - 1 && ","}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        {/*border*/}
+                        <div className="border border-white/20"></div>
+                        {/*button*/}
+                        <div>
+                            
+                        </div>
+                    </div>
+                 </div>
+                <div className="w-full xl:w-[50%]">slider</div>
+            </div>
         </div>
-        </div>
-        </motion.section>
+    </motion.section>
     );
 };
 
